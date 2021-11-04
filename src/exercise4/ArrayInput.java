5c8c8fdc6b5c3ef6096e8fe617b8ac5d545e0211
Diannalp1 <>
2021-11-04 14:58:07 +0000

    mmmm

--- src/exercise4/ArrayInput.java
+++ src/exercise4/ArrayInput.java
@@ -8,7 +8,9 @@ public class ArrayInput {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int[] numbers = new int[3];
+        //int[] numbers = new int[3];
+        int length = input.nextInt();
+        int[] numbers = new int[length];
 
         System.out.println("Enter numbers: ");
         for (int index = 0; index < numbers.length; index++) {
